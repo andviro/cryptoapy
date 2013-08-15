27780e517e72578e49d0a2552c4bbe8f71fcbb61
Andrew Rodionoff <andviro@gmail.com>
2013-08-15 10:35:45 +0400

    убрал дурацкую ошибку в autopem

--- distribute.bat
+++ distribute.bat
@@ -1,8 +1,8 @@
 cd python
 fab swig
 python setup.py bdist_wininst
-copy dist\*.exe windistr\pkgs\
-move dist\*.exe "%HOMEPATH%\Dropbox\Public"
-cd windistr
-"c:\Program Files\NSIS\makensis.exe" install.nsi
-move setup.exe "%HOMEPATH%\Dropbox\Public"
+REM copy dist\*.exe windistr\pkgs\
+REM move dist\*.exe "%HOMEPATH%\Dropbox\Public"
+REM cd windistr
+REM "c:\Program Files\NSIS\makensis.exe" install.nsi
+REM move setup.exe "%HOMEPATH%\Dropbox\Public"
--- python/setup.py
+++ python/setup.py
@@ -118,7 +118,7 @@ csp = Extension('cprocsp._csp',
 
 
 setup(name='cryptoapy',
-      version='0.4.11',
+      version='0.4.12',
       author='Andrew Rodionoff',
       author_email='andviro@gmail.com',
       license='LGPL',
