ee08bf0cd16ab2633645daa8b4700709fa8b6416
Andrew Rodionoff <andviro@gmail.com>
2013-08-21 20:35:23 +0400

    смена версии для загрузки на pypi

--- python/setup.py
+++ python/setup.py
@@ -120,7 +120,7 @@ csp = Extension('cprocsp._csp',
 
 
 setup(name='cryptoapy',
-      version='0.4.19',
+      version='0.4.20',
       author='Andrew Rodionoff',
       author_email='andviro@gmail.com',
       license='LGPL',
