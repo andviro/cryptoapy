d4d9939e639a4517f74abc4aeae687bf7ed28d97
Andrew Rodionoff <andviro@gmail.com>
2013-09-18 12:25:53 +0400

    фикс для питона3

--- python/setup.py
+++ python/setup.py
@@ -120,7 +120,7 @@ csp = Extension('cprocsp._csp',
 
 
 setup(name='cryptoapy',
-      version='0.4.23',
+      version='0.4.24',
       author='Andrew Rodionoff',
       author_email='andviro@gmail.com',
       license='LGPL',
