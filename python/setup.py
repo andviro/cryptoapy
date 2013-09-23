136a6740f6b9b2de44b61abf3d831550d71a7f98
Andrew Rodionoff <andviro@gmail.com>
2013-09-24 03:49:49 +0400

    версия

--- python/setup.py
+++ python/setup.py
@@ -120,7 +120,7 @@ csp = Extension('cprocsp._csp',
 
 
 setup(name='cryptoapy',
-      version='0.4.26',
+      version='0.4.27',
       author='Andrew Rodionoff',
       author_email='andviro@gmail.com',
       license='LGPL',
